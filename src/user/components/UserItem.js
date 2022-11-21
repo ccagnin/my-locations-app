5d98f6f240efe80b9237ed31cbd8c7ce802e09a1
ccagnin <cagnin.caroline@gmail.com>
2022-11-21 17:50:51 -0300

    add styling to users card

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,18 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './UserItem.css'
+import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
 
 const UserItem = props => {
     return (
         <li className="user-item">
-            <div className="user-item__content">
-                <div className="user-item__avatar">
-                    <img src={props.image} alt={props.name} />
-                </div>
-                <div className="user-item__details">
-                    <h2>{props.name}</h2>
-                    <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
-                </div>
-            </div>
+            <Card className="user-item__content">
+                <Link to={`/${props.id}/places`}>
+                    <div className="user-item__avatar">
+                        <Avatar image={props.image} alt={props.name} />
+                    </div>
+                    <div className="user-item__details">
+                        <h2>{props.name}</h2>
+                        <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
+                    </div>
+                </Link>
+            </Card>
         </li>
     )
 };
