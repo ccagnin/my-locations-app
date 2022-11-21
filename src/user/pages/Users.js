f455a229f82880e1c2126a3dd8fa53a5ace63acd
ccagnin <cagnin.caroline@gmail.com>
2022-11-21 17:19:57 -0300

    UsersList Page/Component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-  return <h2>Users Works!</h2>;
+
+  const dummy_users = [
+    {
+      id: 1, 
+      name: 'Cax Cagnin', 
+      image: 'https://images.pexels.com/photos/3978007/pexels-photo-3978007.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', 
+      places: '3' 
+    }
+  ]; 
+
+  return <UsersList items={dummy_users} />
 };
 
 export default Users;
