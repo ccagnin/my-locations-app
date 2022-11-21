85c4fed4abb309712127d80900bbf814f87f5e2a
ccagnin <cagnin.caroline@gmail.com>
2022-11-21 19:19:57 -0300

    feat: drawer state

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MainHeader from './MainHeader';
 import { Link } from 'react-router-dom';
 import NavLinks from './NavLinks';
-
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
-    return <MainHeader>
-        <button className='main-nagivation__menu-btn'>
-            <span />
-            <span />
-            <span />
-        </button>
-        <h1 className='main-navigation__title'>
-            <Link to="/">Your Places</Link>
-        </h1>
-        <nav>
-            <NavLinks />
-        </nav>
-    </MainHeader>
+    const [isSideDrawerOpen, setIsSideDrawerOpen] = useState(false);
+
+    const toggleSideDrawer = () => {
+        setIsSideDrawerOpen(true);
+    };
+
+    const closeSideDrawer = () => {
+        setIsSideDrawerOpen(false);
+    }
+
+    return (
+        <React.Fragment>
+            {isSideDrawerOpen && <Backdrop onClick={closeSideDrawer} /> }
+            {isSideDrawerOpen && <SideDrawer>
+                <nav className="main-navigation__drawer-nav">
+                    <NavLinks />
+                </nav>
+            </SideDrawer>}
+            <MainHeader>
+                <button className='main-nagivation__menu-btn' onClick={toggleSideDrawer}>
+                    <span />
+                    <span />
+                    <span />
+                </button>
+                <h1 className='main-navigation__title'>
+                    <Link to="/">Your Places</Link>
+                </h1>
+                <nav className='main-navigation__header-nav'>
+                    <NavLinks />
+                </nav>
+            </MainHeader>
+        </React.Fragment>
+    );
 };
 
 export default MainNavigation;
